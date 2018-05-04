90d17c5043300545e74eb29dabcf58af15d3f3a3
DESKTOP-UHTAFJN\bitcamp <pms3300@gmail.com>
2018-05-04 12:07:17 +0900

    root-context 설정

--- src/main/java/ncontroller/IndexController.java
+++ src/main/java/ncontroller/IndexController.java
@@ -13,36 +13,36 @@ import vo.Emp;
 
 @Controller
 public class IndexController {
-	
-//		@Autowired
-//		private SqlSession sqlsession;
-		
-		@RequestMapping("/index.htm")
-		public String index() {
-			//return "index.jsp"; 기존방법 (리졸버 없이)
-			System.out.println("타요");
-			return "home.index"; //Tiles 적용하기 <definition name="home.*"
+
+	@Autowired
+	private SqlSession sqlsession;
+
+	@RequestMapping("/index.htm")
+	public String index() {
+		// return "index.jsp"; 기존방법 (리졸버 없이)
+		System.out.println("타요");
+		return "home.index"; // Tiles 적용하기 <definition name="home.*"
+	}
+
+	@RequestMapping("/list.htm")
+	public String allEmpList(Model model) {
+		List<Emp> list = null;
+
+		try {
+			Empdao empdao = sqlsession.getMapper(Empdao.class);
+			list = empdao.getEmpList();
+			model.addAttribute("list", list);
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
-		
-		@RequestMapping("/list.htm")
-		public String allEmpList(Model model) {
-			List<Emp> list = null;
-			
-			try {
-			/*	Empdao empdao =sqlsession.getMapper(Empdao.class);
-				list = empdao.getEmpList();
-				model.addAttribute("list", list);*/
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-			
-			System.out.println("list.htm");
-			return "home.emplist";
-		}
-		
-		@RequestMapping()
-		public String selectByEmpno(String empno) {
-			return "home.emplist";
-		}
-		
+
+		System.out.println("list.htm");
+		return "home.emplist";
+	}
+
+	@RequestMapping()
+	public String selectByEmpno(String empno) {
+		return "home.emplist";
+	}
+
 }
